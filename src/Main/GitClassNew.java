4a2cf349c4856839bb183dc765b786efb76ba627
bffd <ankitagond030@gmail.com>
2022-11-24 21:06:48 +0530

    first

---
+++ src/Main/GitClassNew.java
@@ -0,0 +1,8 @@
+package Main;
+
+public class GitClassNew {
+
+	public static void main(String[] args) {
+		System.out.println("I am in gitClass");
+	}
+}
---
+++ src/Main3/Add_1.java
@@ -0,0 +1,7 @@
+package Main3;
+
+public class Add_1 {
+public static void main(String[] args) {
+	System.out.println("i am in add class");
+}
+}
